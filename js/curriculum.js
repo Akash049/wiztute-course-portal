864702630a12a9c086ce45d8bbb6ac4c5aea8ecf
MelpApp ML <melpml@gmail.com>
2019-12-19 16:02:28 +0530

    First upload

---
+++ js/curriculum.js
@@ -0,0 +1,28 @@
+const off20 = "https://www.instamojo.com/@Wiztute_eLearning/l5926d3eeda4d413db7676dfaac030397/";
+const off10 = "https://www.instamojo.com/@Wiztute_eLearning/lb0a7a92a355844f8a362dd0dec5aea47/"
+$("#apply-coupon-text").click(()=>{
+    $(".coupon-input-box").show()
+    $("#apply-coupon-text").hide();
+    $(".pay-button-full").hide();
+    $(".pay-button-off10").hide();
+    $(".pay-button-off20").hide();
+})
+$("#apply-coupon").click(()=>{
+ let coupon = $("#coupon-value").val();
+ if(coupon == "STDEV436824"){
+     $("#amount-value").text("₹ 54,000 (10% off)");
+     $(".coupon-input-box").hide()
+     $("#apply-coupon-text").show();
+     $(".pay-button-off10").show();
+ }else if(coupon == "STDEV415705"){
+     $("#amount-value").text("₹ 48,000 (20% off)");
+     $(".coupon-input-box").hide()
+     $("#apply-coupon-text").show();
+     $(".pay-button-off20").show();
+ }else{
+     $("#amount-value").text("₹ 60,000");
+     $(".coupon-input-box").hide()
+     $("#apply-coupon-text").show();
+     $(".pay-button-full").show();
+ }
+})
